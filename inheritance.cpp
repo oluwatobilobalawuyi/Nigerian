e2fbded9e5e0290418dc30f6671ff9ebef4675c5
oluwatobilobalawuyi <101818108+oluwatobilobalawuyi@users.noreply.github.com>
2023-09-20 00:52:48 -0500

    Create inheritance.cpp
    this is the inheritance C++ code.

---
+++ inheritance.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Person class
+class Person {
+protected:
+    string name;
+    int age;
+public:
+    Person(string n, int a) : name(n), age(a) {}
+    virtual void printDetails() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+// Nigerian class (inherits from Person)
+class Nigerian : public Person {
+private:
+    string tribe;
+public:
+    Nigerian(string n, int a, string t) : Person(n, a), tribe(t) {}
+    void printDetails() override {
+        cout << "--- Nigerian Details ---" << endl;
+        Person::printDetails();
+        cout << "Tribe: " << tribe << endl;
+    }
+};
+
+// Foreigner class (inherits from Person)
+class Foreigner : public Person {
+private:
+    bool passport;
+public:
+    Foreigner(string n, int a, bool p) : Person(n, a), passport(p) {}
+    void printDetails() override {
+        cout << "--- Foreigner Details ---" << endl;
+        Person::printDetails();
+        if (passport)
+            cout << "Has passport" << endl;
+        else
+            cout << "Does not have passport" << endl;
+    }
+};
+
+int main() {
+    Person* people[4];
+
+    people[0] = new Nigerian("Odoqgu Nwazuoroke", 12, "Port Harcourt");
+    people[1] = new Foreigner("Jane Smith", 30, true);
+    people[2] = new Nigerian("Awon Werey", 69, "Yoruba");
+    people[3] = new Person("Rashidi Makoko", 10);
+
+    for (int i = 0; i < 4; i++) {
+        people[i]->printDetails();
+        cout << endl;
+    }
+
+    // free memory
+    for (int i = 0; i < 4; i++) {
+        delete people[i];
+    }
+    return 0;
+}
